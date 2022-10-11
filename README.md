7286518ce26f6671c063d2b2367a2d861f51006d
Blake Scherschel <blake.scherschel@gmail.com>
2022-10-10 17:44:17 -0700

    Added ability to install from anywhere with just one line

--- README.md
+++ README.md
@@ -5,15 +5,6 @@ Some development utilities that I am willing to share with others publicly.
 # How-To Install
 
 ```
-#!/usr/bin/sh
-
-# Download repo to /tmp/
-mkdir -p /tmp/bscher-dev-utils
-cd /tmp/bscher-dev-utils
-git clone git@github.com:bscher/dev-utils.git
-
-# Run install script
-cd /tmp/bscher-dev-utils/dev-utils/
-python3 ./install.py
+curl https://raw.githubusercontent.com/bscher/dev-utils/main/install_from_anywhere.sh | sh
 ```
 
---
+++ install_from_anywhere.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+set -e
+
+# Expect Python3 and Git to be installed
+if ! command -v git >/dev/null; then
+    echo "Failed to find 'git' installed" >&2
+    exit 1
+fi
+if ! command -v python3 >/dev/null; then
+    echo "Failed to find 'python3' installed" >&2
+    exit 1
+fi
+
+mkdir -p /tmp/bscher-dev-utils
+
+# Download repo to /tmp/
+mkdir -p /tmp/bscher-dev-utils
+cd /tmp/bscher-dev-utils
+rm -rf ./dev-utils || /usr/bin/true
+git clone git@github.com:bscher/dev-utils.git
+
+# Run install script
+cd /tmp/bscher-dev-utils/dev-utils/
+python3 ./install.py
